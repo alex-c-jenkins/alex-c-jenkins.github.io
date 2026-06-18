382ba074a8caae9314fd5b6e7d788b79fec6fb3d
alex-c-jenkins <acj46@cam.ac.uk>
2026-06-18 09:54:15 +0000

    Deploying to gh-pages from @ alex-c-jenkins/alex-c-jenkins.github.io@a1ba8e4e172b13e9aaa773e4ed84bf2c6fc8f291 🚀

--- wc2026.py
+++ wc2026.py
@@ -1,19 +1,20 @@
 import json
 import numpy as np
 from datetime import datetime
-from os import system
+from os import chdir, getcwd, system
 
-dir = '/Users/alex/Documents/Work/alex-c-jenkins.github.io/'
+cwd = getcwd()
+chdir('/Users/alex/Documents/Work/alex-c-jenkins.github.io/')
 
-system('cp ' + dir + 'assets/json/wc2026.json '
-       + dir + 'assets/json/wc2026.json.bak')
-system('/opt/homebrew/bin/claudinho --json --no-color --flavor off table > '
-       + dir + 'assets/json/wc2026.json')
+system('cp assets/json/wc2026.json assets/json/wc2026.json.bak')
+system('/opt/homebrew/bin/node '
+       + '/opt/homebrew/lib/node_modules/@claudinho/cli/dist/index.js '
+       + '--json --no-color --flavor off table > assets/json/wc2026.json')
 
-with open(dir + 'assets/json/wc2026.json', 'r') as f:
+with open('assets/json/wc2026.json', 'r') as f:
     data = json.load(f)['tables']
 
-with open(dir + 'assets/json/wc2026.json.bak', 'r') as f:
+with open('assets/json/wc2026.json.bak', 'r') as f:
     old_data = json.load(f)['tables']
 
 if data != old_data:
@@ -83,7 +84,7 @@ if data != old_data:
     goaldiff = goaldiff[ids]
     scores = scores[ids]
 
-    file = open(dir + '_pages/wc2026.md', 'w')
+    file = open('_pages/wc2026.md', 'w')
     file.write('---\n')
     file.write('layout: page\n')
     file.write('title: Jenkins World Cup Sweepstake 2026\n')
@@ -107,7 +108,8 @@ if data != old_data:
     file.close()
 
     system('git commit -m "Update WC2026" '
-           + dir + 'assets/json/wc2026.json ' + dir + '_pages/wc2026.md')
+           + 'assets/json/wc2026.json _pages/wc2026.md')
     system('git push')
 
-system('rm ' + dir + 'assets/json/wc2026.json.bak')
+system('rm assets/json/wc2026.json.bak')
+chdir(cwd)
